4ac691926badb6a21b497d557051eec6e334e7e2
leandervanderbiesen <leandervanderbiesen@gmail.com>
2025-12-27 17:09:14 +0100

    feat: add Navigation & About component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Navigation from '@/components/sections/Navigation';
+import About from '@/components/sections/About';
+
 export default function Home() {
   return (
-  <div></div>
+    <div className="min-h-screen bg-background text-foreground">
+      <Navigation />
+      <About />
+    </div>
   );
 }

c5a0ee7b25486840a1c1368f440495173958c648
leandervanderbiesen <leandervanderbiesen@gmail.com>
2025-12-31 15:11:17 +0100

    refactor: move download resume function to utils file

--- components/sections/Navigation.tsx
+++ components/sections/Navigation.tsx
@@ -2,8 +2,11 @@
 
 import Button from '@/components/ui/Button';
 import { useEffect, useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import MobileMenu from '@/components/ui/MobileMenu';
+import { downloadResume } from '@/lib/utils';
 
-const navLinks = [
+export const navLinks = [
   {
     text: 'About',
     href: '#about',
@@ -41,53 +44,26 @@ const Navigation = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const downloadResume = async () => {
-    try {
-      const response = await fetch('/CV_Leander_Vanderbiesen.pdf');
-
-      if (!response.ok) {
-        throw new Error('Download failed');
-      }
-
-      const blob = await response.blob();
-
-      const url = window.URL.createObjectURL(blob);
-
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'CV_leander_vanderbiesen.pdf';
-      document.body.appendChild(a);
-      a.click();
-
-      window.URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    } catch (e) {
-      console.error('Something went wrong downloading file:', e);
-    }
-  };
-
   return (
     <nav
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'glass py-4' : 'py-6'}`}
     >
-      <div className="container-narrow flex items-center justify-between">
+      <div className="container-narrow flex items-center justify-between px-4 xl:px-0">
         <div>
           <a href="#" className="text-xl font-mono font-bold text-gradient">
             &lt;dev /&gt;
           </a>
         </div>
         <div className="hidden md:flex items-center gap-8">
-          {navLinks.map(link => {
-            return (
-              <a
-                key={link.href}
-                href={link.href}
-                className="text-muted-foreground hover:text-primary transition-colors duration-300 text-sm font-medium"
-              >
-                {link.text}
-              </a>
-            );
-          })}
+          {navLinks.map(link => (
+            <a
+              key={link.href}
+              href={link.href}
+              className="text-muted-foreground hover:text-primary transition-colors duration-300 text-sm font-medium"
+            >
+              {link.text}
+            </a>
+          ))}
           <Button variant="outline" size="sm" onClick={downloadResume}>
             Resume
           </Button>
@@ -98,8 +74,9 @@ const Navigation = () => {
           className="md:hidden"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
         >
-          {!isMobileMenuOpen && <p>mobile</p>}
+          {isMobileMenuOpen ? <X /> : <Menu />}
         </Button>
+        {isMobileMenuOpen && <MobileMenu />}
       </div>
     </nav>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function downloadResume() {
+  try {
+    const response = await fetch('/CV_Leander_Vanderbiesen.pdf');
+
+    if (!response.ok) {
+      throw new Error('Download failed');
+    }
+
+    const blob = await response.blob();
+
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'CV_leander_vanderbiesen.pdf';
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  } catch (e) {
+    console.error('Something went wrong downloading file:', e);
+  }
+}

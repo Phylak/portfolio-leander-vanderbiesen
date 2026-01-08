2376b1497010b7f8aaccbc207ab15d614ccafe69
leandervanderbiesen <leandervanderbiesen@gmail.com>
2026-01-08 13:31:58 +0100

    feat: create github & ai skeleton component

---
+++ components/sections/AI.tsx
@@ -0,0 +1,13 @@
+import SectionHeading from '../ui/SectionHeading';
+
+const AISection: React.FC = () => {
+  return (
+    <section id="askAI" className="section-padding">
+      <div className="container-narrow">
+        <SectionHeading count="03" title="Ask AI" />
+      </div>
+    </section>
+  );
+};
+
+export default AISection;
---
+++ components/sections/Github.tsx
@@ -0,0 +1,13 @@
+import SectionHeading from '../ui/SectionHeading';
+
+const Github: React.FC = () => {
+  return (
+    <section id="github" className="section-padding">
+      <div className="container-narrow">
+        <SectionHeading count="04" title="Github Contributions" />
+      </div>
+    </section>
+  );
+};
+
+export default Github;

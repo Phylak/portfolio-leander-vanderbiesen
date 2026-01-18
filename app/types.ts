378dc5b7a2230a5655fdb9150d99913520c2d4e1
leandervanderbiesen <leandervanderbiesen@gmail.com>
2026-01-18 16:49:00 +0100

    feat: add enum for ai models

--- app/types.ts
+++ app/types.ts
@@ -23,3 +23,13 @@ type ContributionDay = {
   weekday: number;
   color: string;
 };
+
+export enum AIModelValue {
+  GEMINI_FLASH_LATEST = 'gemini-flash-latest',
+  GEMINI_FLASH_LITE_LATEST = 'gemini-flash-lite-latest',
+  GEMINI_3_PRO_PREVIEW = 'gemini-3-pro-preview',
+  GEMINI_3_FLASH_PREVIEW = 'gemini-3-flash-preview',
+  GEMINI_2_5_FLASH = 'gemini-2.5-flash',
+  GEMINI_2_5_PRO = 'gemini-2.5-pro',
+  GEMINI_2_5_FLASH_LITE = 'gemini-2.5-flash-lite',
+}

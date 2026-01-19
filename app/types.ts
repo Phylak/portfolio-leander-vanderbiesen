dc1fe10823a496f22270322096ca6594372ce8e3
leandervanderbiesen <leandervanderbiesen@gmail.com>
2026-01-19 16:17:47 +0100

    refactor: use type instead of enum for AIModel property

--- app/types.ts
+++ app/types.ts
@@ -24,12 +24,7 @@ type ContributionDay = {
   color: string;
 };
 
-export enum AIModelValue {
-  GEMINI_FLASH_LATEST = 'gemini-flash-latest',
-  GEMINI_FLASH_LITE_LATEST = 'gemini-flash-lite-latest',
-  GEMINI_3_PRO_PREVIEW = 'gemini-3-pro-preview',
-  GEMINI_3_FLASH_PREVIEW = 'gemini-3-flash-preview',
-  GEMINI_2_5_FLASH = 'gemini-2.5-flash',
-  GEMINI_2_5_PRO = 'gemini-2.5-pro',
-  GEMINI_2_5_FLASH_LITE = 'gemini-2.5-flash-lite',
-}
+export type AIModelValue = {
+  value: string;
+  label: string
+};

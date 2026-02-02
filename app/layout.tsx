9cb9e50775cf58b032e028f559474aab4fa00e8d
leandervanderbiesen <leandervanderbiesen@gmail.com>
2026-02-02 14:12:53 +0100

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Leander Vanderbiesen | Frontend Developer in Zürich',
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
